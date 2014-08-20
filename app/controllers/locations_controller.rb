55da7af8db0626f3c5d59faac5d28e106a9a0fe0
catburston <cat1788@gmail.com>
2014-08-20 16:16:09 +0200

    add Create Edit Delete actions to Location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,12 +1,61 @@
 class LocationsController < ApplicationController
+  before_filter :authenticate_user!, except: [:index, :show]
 
   def index
     @user = current_user
     @locations = Location.where(id: params[:ids])
+
+    if params[:user_id]
+      @locations = Location.where(:user_id => params[:user_id])
+    end
   end
 
   def show
     @user = current_user
+    @location = Location.find(params[:id])
+  end
+
+  def new
+    @user = current_user
+    @location = Location.new
+  end
+
+  def create
+    @user = current_user
+    @location = Location.new location_params
+    @location.user = current_user
+    if @location.save
+      redirect_to location_path(@location.id), notice: 'Location created'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = current_user
+    @location = Locaiton.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    location = @user.locations.find(params[:id]).destroy
+    redirect_to action: 'index'
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @location = @user.locations.find(params[:id])
+    if @location.update_attributes location_params
+      redirect_to location_path(@location.id), notice: 'Location successfully updated'
+    else
+      # @errors = @location.errors.full_messages
+      render 'edit'
+    end
+  end
+
+  private
+  def location_params
+    params.require(:location).permit(:street_name, :suburb, :post_code, :city, :state, :country)
   end
 
 end

e19b6887a4923736ceb9b3e9113ea8d0a982f9ca
catburston <cat1788@gmail.com>
2014-08-20 15:55:08 +0200

    update locations controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,12 @@
 class LocationsController < ApplicationController
 
   def index
+    @user = current_user
     @locations = Location.where(id: params[:ids])
+  end
+
+  def show
+    @user = current_user
   end
 
 end

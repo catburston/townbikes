f0bdb5eccc5d27e326044a6285e14a53508276ac
catburston <cat1788@gmail.com>
2014-08-23 00:51:09 +0200

    add modal login and sign_up

--- app/uploaders/bicycle_photos_uploader.rb
+++ app/uploaders/bicycle_photos_uploader.rb
@@ -25,6 +25,10 @@ class BicyclePhotosUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg gif png)
   end
 
+  def default_url
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "tandem-default.png"].compact.join('_'))
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:

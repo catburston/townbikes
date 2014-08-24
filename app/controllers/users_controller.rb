5f0146c845642c91d8b6972cb0a62b9c6ca529f0
catburston <cat1788@gmail.com>
2014-08-24 21:24:16 +0200

    refactor user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @bicycles = @user.bicycles      # Why do I need this? Otherwise @user.bicycles in the show doesn't work
   end
 end

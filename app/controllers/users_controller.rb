f315a280a5939165c7126fcad7fccf93180c130a
catburston <cat1788@gmail.com>
2014-08-24 18:27:47 +0200

    update style of user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @bicycles = @user.bicycles
-    @location = Location.find_by(:user_id => @user.id)
   end
 end

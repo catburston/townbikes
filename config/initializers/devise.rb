872ba62901d026656076107b57b6ca0f62f94783
catburston <cat1788@gmail.com>
2014-08-25 22:25:05 +0200

    more sendgrid config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'cat1788@gmail.com'
+  config.mailer_sender = 'townbikesapp@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'

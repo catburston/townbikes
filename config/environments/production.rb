e0d420761806ef8b47456c3a2a1b04cb3719a6e5
catburston <cat1788@gmail.com>
2014-08-25 22:34:46 +0200

    sendgrid url config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'}
+  config.action_mailer.default_url_options = { :host => 'townbikesapp.herokuapp.com'}
   # config.action_mailer.smtp_settings = {
   #   address:              'smtp.gmail.com',
   #   port:                 587,

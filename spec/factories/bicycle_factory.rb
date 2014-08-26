6e0650f19f93027b38a849a205b57af6e649e1b2
catburston <cat1788@gmail.com>
2014-08-27 01:23:45 +0200

    fix bicycle controller spec

--- spec/factories/bicycle_factory.rb
+++ spec/factories/bicycle_factory.rb
@@ -6,6 +6,7 @@ FactoryGirl.define do
     b.bicycle_type "commuter"
     b.size { Faker::Number.number(2) }
     b.daily_cost { Faker::Number.number(2) }
+    b.properties "ladies"=>"0","helmet"=>"1", "front_basket"=>"1", "rear_basket"=>"0", "trailer"=>"0", "internal_gears"=>"0", "child_seat"=>"0", "kickstand"=>"0", "safety_vest"=>"0", "panniers"=>"0", "rack"=>"0", "fenders"=>"0", "lock"=>"1", "skirt_guard"=>"0", "pump"=>"0", "toolkit"=>"0"
     b.description { Faker::Lorem.paragraph(2) }
     b.user { FactoryGirl.create(:user) }
   end

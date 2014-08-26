a1cb91e97d1dd834db7fe8571d31ea6ec7092781
catburston <cat1788@gmail.com>
2014-08-27 00:37:09 +0200

    another try for heroku asstes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
